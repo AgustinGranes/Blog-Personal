7c925c41c8a9f70c00bfb35a5b862b7a1a53ebe8
Your Name <you@example.com>
2025-08-07 15:19:43 -0300

    SI TE HE FALLADO TE PIDO PERDON DE LA UNICA FORMA QUE SE

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -4,6 +4,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     pub_date = models.DateTimeField('date published')
+    cover = models.ImageField(upload_to='covers/', blank=True, null=True)
 
     def __str__(self):
         return self.title
